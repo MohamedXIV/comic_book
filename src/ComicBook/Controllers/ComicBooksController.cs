6e3ce03048c8b761369ed22d6f9c5975b6437683
Mohamed Farag <mohamedxiv@gmail.com>
2019-04-27 13:33:02 +0200

    Added design files

--- src/ComicBook/Controllers/ComicBooksController.cs
+++ src/ComicBook/Controllers/ComicBooksController.cs
@@ -10,6 +10,26 @@ namespace ComicBook.Controllers
     {
         public ActionResult Detail()
         {
+            ViewBag.SeriesTitle = "Batman";
+            ViewBag.IssueNumber = 101;
+            ViewBag.Description = "Batman Family, Batman Incorporated," +
+                        " Justice League, Outsiders, Wayne Enterprises," +
+                        " Club of Heroes, formerly White Lantern Corps," +
+                        " Sinestro Corps, relatives:Damian Wayne (son)," +
+                        " Dick Grayson (adopted son), Tim Drake (adopted son)," +
+                        " Jason Todd (adopted son), Cassandra Cain (adopted ward)\nMartha Wayne" +
+                        " (mother, deceased), Thomas Wayne (father, deceased), Alfred Pennyworth" +
+                        " (former guardian), Roderick Kane (grandfather, deceased), Elizabeth Kane" +
+                        " (grandmother, deceased), Nathan Kane (uncle, deceased), Simon Hurt (ancestor), Wayne Family";
+            ViewBag.Artists = new string[]
+            {
+                "Sripts: Not Me",
+                "Pencils: Not Me",
+                "Inks: Not Me",
+                "Colors: Not Me",
+                "Letters: Not Me"
+            };
+
             return View();
         }
     }
